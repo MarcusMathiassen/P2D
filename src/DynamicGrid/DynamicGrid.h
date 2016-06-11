82de4e9adb38966dda74caf5b65901769f4f5aa5
MarcusMathiassen <mathiassenmarcus@gmail.com>
2016-06-11 03:51:49 +0200

    Removed some comments

--- src/DynamicGrid/DynamicGrid.h
+++ src/DynamicGrid/DynamicGrid.h
@@ -21,51 +21,6 @@
 // ----------------------------------------------------------------------
 
 
-/*
-
-	HOW THE DYNAMIC GRID WORKS:
-
-	---------------------------------------------------------------------
-
-	PROGRAM START AND EVERY SCREEN RESIZE: 
-
-	init():
-	---------------------------------------------------------------------
-	The nodes are cleared and given an element in the grid.
-	---------------------------------------------------------------------
-
-
-	EACH FRAME(in order): 
-	
-	clear(): 
-	---------------------------------------------------------------------
-	Clears all nodes and their objects. (If we didn´t do this, each nodes
-	object-container would increase in size each frame, basically 
-	leaking memory.)
-	---------------------------------------------------------------------
-
-	update(): 
-	---------------------------------------------------------------------
-	Goes through every node and fills it with objects from the 
-	main-container, any object that fits within the nodes boundaries will
-	be added to the nodes object-container.
-	---------------------------------------------------------------------
-
-	process(): 
-	---------------------------------------------------------------------
-	Checks if any objects in the node collide with eachother, and if so, 
-	resolves those collision.
-	---------------------------------------------------------------------
-
-	draw():	
-	---------------------------------------------------------------------
-	Draws the nodes boundaries to screen and colors the objects within 
-	each node with the nodes color.
-	---------------------------------------------------------------------
-
-*/
-
-
 class DynamicGrid
 {
 
