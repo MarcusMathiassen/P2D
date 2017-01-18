bb42f70eed84159632efaf12b10ab6a601bf9aa4
Marcus Mathiassen <marcusmathiassen@client-80-161.wireless.hig.no>
2017-01-18 03:53:30 +0100

    Beautified

--- include/hwinfo.h
+++ include/hwinfo.h
@@ -2,13 +2,12 @@
 #define HWINFO_H
 
 #include <string>
-#include <sys/types.h>
 #include <sys/sysctl.h>
+#include <sys/types.h>
 
-uint64_t 		get_cpu_freq();
-uint64_t 		get_cpu_cores();
-uint64_t 		get_cpu_threads();
-std::string 	get_cpu_brand();
-
+uint64_t get_cpu_freq();
+uint64_t get_cpu_cores();
+uint64_t get_cpu_threads();
+std::string get_cpu_brand();
 
 #endif
--- src/hwinfo.cpp
+++ src/hwinfo.cpp
@@ -1,47 +1,40 @@
 #include "hwinfo.h"
 
-uint64_t get_cpu_freq()
-{
-    uint64_t num = 0;
-    size_t size = sizeof(num);
+uint64_t get_cpu_freq() {
+  uint64_t num = 0;
+  size_t size = sizeof(num);
 
-    if (sysctlbyname("hw.cpufrequency", &num, &size, NULL, 0) < 0)
-    {
-        perror("sysctl");
-    }
-    return num;
+  if (sysctlbyname("hw.cpufrequency", &num, &size, NULL, 0) < 0) {
+    perror("sysctl");
+  }
+  return num;
 }
 
-uint64_t get_cpu_cores()
-{
-    uint64_t num = 0;
-    size_t size = sizeof(num);
+uint64_t get_cpu_cores() {
+  uint64_t num = 0;
+  size_t size = sizeof(num);
 
-    if (sysctlbyname("hw.physicalcpu_max", &num, &size, NULL, 0) < 0)
-    {
-        perror("sysctl");
-    }
-    return num;
+  if (sysctlbyname("hw.physicalcpu_max", &num, &size, NULL, 0) < 0) {
+    perror("sysctl");
+  }
+  return num;
 }
 
-uint64_t get_cpu_threads()
-{
-    uint64_t num = 0;
-    size_t size = sizeof(num);
+uint64_t get_cpu_threads() {
+  uint64_t num = 0;
+  size_t size = sizeof(num);
 
-    if (sysctlbyname("hw.logicalcpu_max", &num, &size, NULL, 0) < 0)
-    {
-        perror("sysctl");
-    }
-    return num;
+  if (sysctlbyname("hw.logicalcpu_max", &num, &size, NULL, 0) < 0) {
+    perror("sysctl");
+  }
+  return num;
 }
 
-std::string get_cpu_brand()
-{
-    char buffer[128];
-    size_t bufferlen = 128;
+std::string get_cpu_brand() {
+  char buffer[128];
+  size_t bufferlen = 128;
 
-    sysctlbyname("machdep.cpu.brand_string",&buffer,&bufferlen,NULL,0);
+  sysctlbyname("machdep.cpu.brand_string", &buffer, &bufferlen, NULL, 0);
 
-    return buffer;
+  return buffer;
 }
